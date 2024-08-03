83e6a9b968d38d25f79d546a19f515448c366e61
Vlad <vladislav.musaelyan@renesandro.info>
2024-08-03 11:28:05 +0300

    added input file path in console

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import asyncio
 import aiohttp
 import aiofiles
+import argparse
 
-
-FILE_WITH_URLS_PATH = "urls.txt"
 TIMEOUT_TIME = 0.51  # AVG value for seeing print statements about timing
 
 
-def get_urls_from_file():
-    with open(FILE_WITH_URLS_PATH, "r") as file:
-        urls = [line.strip() for line in file]
+async def get_urls_from_file(file_path: str):
+    async with aiofiles.open(file_path, 'r') as file:
+        urls = [line.strip() for line in await file.readlines()]
     return urls
 
 
@@ -34,24 +33,28 @@ async def get_information_from_url_with_timeout(url: str, file_number: int):
         print("Getting information from url timed out, moving on...")
 
 
-async def test1():
+async def test1(file_path: str):
     """Default, with one cycle"""
-    urls = get_urls_from_file()
+    urls = await get_urls_from_file(file_path)
     tasks = [get_information_from_url_with_timeout(url, number) for number, url in enumerate(urls)]
     await asyncio.gather(*tasks)
 
 
-async def test2():
+async def test2(file_path: str):
     """With ten cycles, sometimes we can see print statements about timed out, but be careful with that test :)."""
-    urls = get_urls_from_file()
+    urls = await get_urls_from_file(file_path)
     tasks = [get_information_from_url_with_timeout(url, number) for number, url in enumerate(urls * 10)]
     await asyncio.gather(*tasks)
 
 
-async def main():
-    await test1()
-    # await test2()
+async def main(file_path: str):
+    await test1(file_path)
+    # await test2(file_path)
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    parser = argparse.ArgumentParser(description="Fetch URLs and save content to files.")
+    parser.add_argument("file_path", help="Path to the file containing URLs")
+    args = parser.parse_args()
+
+    asyncio.run(main(args.file_path))
